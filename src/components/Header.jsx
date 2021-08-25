9bfea6c4b7fdb4eb4d74f922eca87c09f61bdc1c
serhio46 <1Serhio46@gmail.com>
2021-08-25 22:57:34 +0300

    fix:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
 		<div className="header">
 			<div className="header__logo">Note editor</div>
 			<div className="search">
-				<img height={30} width={30} src="/img/search.png" alt="Лупа" className="search__img" />
+				<img height={30} width={30} src="img/search.png" alt="Лупа" className="search__img" />
 				<input type="text" placeholder="Поиск по тегу" className="search__input input" />
-				<img height={20} width={20} src="/img/close.png" alt="Крестик" className="search__close" />
+				<img height={20} width={20} src="img/close.png" alt="Крестик" className="search__close" />
 			</div>
 		</div>
 	);
